669d25568ff4d70c16f633b5556b86a729823f02
Gabe <gabeorlanski@gmail.com>
2022-04-12 15:08:41 -0400

    Removed dumb checks

--- tio/common.py
+++ tio/common.py
@@ -4,10 +4,6 @@ from typing import List
 PROJECT_ROOT = Path(__file__).parents[1]
 FIXTURES_ROOT = PROJECT_ROOT.joinpath("test_fixtures")
 
-# Check if we are in the home dir of the repo.
-assert PROJECT_ROOT.joinpath("LICENSE.md").exists()
-
-
 def prepare_references_for_bleu(references: List):
     if isinstance(references[0], list):
         references = [[t for t in target] for target in references]
